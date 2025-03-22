353f7bf6e7856d669489456d62edda1e089f38cd
Mariano Vera Causich <95258268+MarianoVera22@users.noreply.github.com>
2025-03-22 20:35:25 -0300

    Excercise NumPy 57

--- NumPy-Basic.py
+++ NumPy-Basic.py
@@ -422,4 +422,342 @@
 
 #----------------Exercise 36----------------
 
+# import numpy as np
+# import os
+
+# x = np.arange(10)
+# y = np.arange(11, 20)
+
+# print("Original arrays:")
+# print(x)
+# print(y)
+
+# np.savez('temp_arra.npz', x=x, y=y)
+# print("Load arrays from the 'temp_arra.npz' file:")
+
+# with np.load('temp_arra.npz') as data:
+#     x2 = data['x']
+#     y2 = data['y']
+    
+#     print(x2)
+#     print(y2) 
+    
+#----------------Exercise 37----------------
+
+# import numpy as np
+
+# x = np.arange(12).reshape(4, 3)
+# print("Original array:")
+# print(x)
+    
+# header = 'col1 col2 col3'
+# np.savetxt('temp.txt', x, fmt="%d", header=header)
+
+# print("After loading, content of the text file:")
+# result = np.loadtxt('temp.txt')
+# print(result)
+
+#----------------Exercise 38----------------
+
+# import numpy as np
+# import os
+
+# x = np.array([1, 2, 3, 4, 5, 6])
+# print("Original array:")
+# print(x)
+
+# x_bytes = x.tobytes()
+# x2 = np.frombuffer(x_bytes, dtype=x.dtype)
+
+# print("After loading, content of the text file:")
+# print(x2)
+
+# print(np.array_equal(x, x2)) 
+
+#----------------Exercise 39----------------
+
+# import numpy as np
+
+# x = [[1, 2], [3, 4]]
+
+# y = np.array(x)
+
+# x2 = y.tolist()
+
+# print(x==x2)
+
+#----------------Exercise 40----------------
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.arange(0, 3 * np.pi, 0.2)
+# y = np.sin(x)
+
+# print("Original array:")
+# print(x)
+
+# print("Plot the points using matplotlib:")
+# plt.plot(x, y)
+# plt.show() 
+
+#----------------Exercise 41----------------
+
+# import numpy as np
+
+# print("numpy.float32 to python float")
+# x = np.float32(0)
+# print(type(x))
+
+# pyval = x.item()
+# print(type(pyval)) 
+
+#----------------Exercise 42----------------
+
+# import numpy as np
+
+# def sum_matrix_Elements(m):
+#     a=np.array(m)
+#     sum_element=0
+    
+#     for i in range(len(a)):
+#         for j in range(len(a[i])):
+#             if a[i][j]==0 and i<len(a)-1:
+#                 a[i+1][j]=0
+                
+#             sum_element+=a[i][j]
+
+#     return sum_element
+
+
+
+# m = [[1, 1, 0, 2],
+#      [0, 3, 0, 3],
+#      [1, 0, 4, 4]]
+
+# print("Original matrix:")
+# print(m)
+
+# print("Sum:")
+# print(sum_matrix_Elements(m)) 
+
+#----------------Exercise 43----------------
+
+# import numpy as np
+
+# nums = np.array([[3, 2, np.nan, 1],
+#                  [10, 12, 10, 9],
+#                  [5, np.nan, 1, np.nan]])
+
+# print("Original array:")
+# print(nums)
+
+# print("\nFind the missing data of the said array:")
+# print(np.isnan(nums)) 
+
+#----------------Exercise 44----------------
+
+# import numpy as np
+
+# nums1 = np.array([0.5, 1.5, 0.2])
+# nums2 = np.array([0.4999999999, 1.500000000, 0.2])
+
+# np.set_printoptions(precision=15)
+# print("Original arrays:")
+# print(nums1)
+# print(nums2)
+
+# print("\nTest said two arrays are equal (element wise) or not:?")
+# print(nums1 == nums2)
+
+#----------------Exercise 45----------------
+
+# import numpy as np
+
+# nums = np.arange(1, 21)
+# print("One-dimensional array of single digit numbers:") 
+# print(nums)
+
+# nums = np.arange(10, 21)
+# print("\nOne-dimensional array of two digit numbers:") 
+# print(nums)
+
+# nums = np.arange(100, 201)
+# print("\nOne-dimensional array of three digit numbers:") 
+# print(nums)
+
+#----------------Exercise 46----------------
+
+# import numpy as np
+
+# print("Create an array of shape (15,10):") 
+
+# print("Command-1")
+# print(np.arange(1, 151).reshape(15, 10)) 
+
+# print("\nCommand-2")
+# print(np.arange(1, 151).reshape(-1, 10)) 
+
+# print("\nCommand-3")
+# print(np.arange(1, 151).reshape(15, -1))
+
+#----------------Exercise 47----------------
+
+# import numpy as np
+
+# np.random.seed(10)
+# print(np.random.rand(40)) 
+
+
+#----------------Exercise 48----------------
+
+# import numpy as np
+
+# np.random.seed(20) 
+
+# cbrt = np.cbrt(7)
+# nd1 = 200 
+# print(cbrt * np.random.randn(8, 5) + nd1) 
+
+#----------------Exercise 49----------------
+
+# import numpy as np
+
+# print("Generate a uniform random sample with replacement:") 
+# print(np.random.choice(7, 5))
+
+# print("\nGenerate a uniform random sample without replacement:") 
+# print(np.random.choice(7, 5, replace=False))
+
+# print("\nGenerate a non-uniform random sample with replacement:")
+# print(np.random.choice(7, 5, p=[0.1, 0.2, 0, 0.2, 0.4, 0, 0.1]))
+
+# print("\nGenerate a uniform random sample without replacement:")
+# print(np.random.choice(7, 5, replace=False, p=[0.1, 0.2, 0, 0.2, 0.4, 0, 0.1])) 
+
+#----------------Exercise 50----------------
+
+# import numpy as np
+
+# nums = np.arange(16, dtype='int').reshape(-1, 4)
+# print("Original array:")
+# print(nums)
+
+# nums[[0,-1],:] = nums[[-1,0],:]
+# print("\nNew array after swapping first and last rows of the said array:")
+# print(nums) 
+
+#----------------Exercise 51----------------
+
+# import numpy as np
+
+# nums = np.zeros(shape=(5, 6), dtype='int')
+# print("Original array:")
+# print(nums)
+
+# nums[::2, ::2] = 3
+# nums[1::2, ::2] = 7
+
+# print("\nNew array:")
+# print(nums) 
+
+#----------------Exercise 52----------------
+
+# import numpy as np
+
+# nums = np.array([[5.54, 3.38, 7.99],
+#               [3.54, 4.38, 6.99],
+#               [1.54, 2.39, 9.29]])
+# print("Original array:")
+# print(nums)
+
+# print("\nSort the said array by row in ascending order:")
+# print(np.sort(nums, axis=1))
+
+# print("\nSort the said array by column in ascending order:")
+# print(np.sort(nums, axis=0)) 
+
+#----------------Exercise 53----------------
+
+# import numpy as np
+
+# nums = np.array([[5.54, 3.38, 7.99],
+#               [3.54, 4.38, 6.99],
+#               [1.54, 2.39, 9.29]])
+# print("Original array:")
+# print(nums)
+
+# n = 5
+# print("\nElements of the said array greater than", n)
+# print(nums[nums > n])
+
+# n = 6
+# print("\nElements of the said array less than", n)
+# print(nums[nums < n]) 
+
+#----------------Exercise 54----------------
+
+# import numpy as np
+
+# nums = np.array([[5.54, 3.38, 7.99],
+#               [3.54, 8.32, 6.99],
+#               [1.54, 2.39, 9.29]])
+# print("Original array:")
+# print(nums)
+
+# n = 8.32
+# r = 18.32
+
+# print("\nReplace elements of the said array which are equal to", n, "with", r)
+# print(np.where(nums == n, r, nums))
+
+# print("\nReplace elements of the said array which are less than", n, "with", r)
+# print(np.where(nums < n, r, nums))
+
+# print("\nReplace elements of the said array which are greater than", n, "with", r)
+# print(np.where(nums > n, r, nums))
+
+
+#----------------Exercise 55----------------
+
+# import numpy as np
+
+# nums = np.array([[5.54, 3.38, 7.99],
+#               [3.54, 8.32, 6.99],
+#               [1.54, 2.39, 9.29]])
+# print("Original array:")
+# print(nums)
+
+# print("\nNew array of equal shape and data type of the said array filled by 0:")
+# print(np.zeros_like(nums))
+
+#----------------Exercise 56----------------
+
+# import numpy as np
+
+# nums = np.array([[[1, 5, 2, 1],
+#                [4, 3, 5, 6],
+#                [6, 3, 0, 6],
+#                [7, 3, 5, 0],
+#                [2, 3, 3, 5]],
+              
+#               [[2, 2, 3, 1],
+#                [4, 0, 0, 5],
+#                [6, 3, 2, 1],
+#                [5, 1, 0, 0],               
+#                [0, 1, 9, 1]],
+              
+#               [[3, 1, 4, 2],
+#                [4, 1, 6, 0],
+#                [1, 2, 0, 6],
+#                [8, 3, 4, 0],               
+#                [2, 0, 2, 8]]]) 
+
+# print("Array:")
+# print(nums)
+
+#----------------Exercise 57----------------
+
 import numpy as np
+
+
